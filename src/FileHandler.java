a4d39c715ba6f9cc8d293c9ba902c37138963a76
Cebix90 <cebulmat@gmail.com>
2023-12-08 22:35:50 +0100

    writeUserInputToFile method added

---
+++ src/FileHandler.java
@@ -0,0 +1,68 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+interface IFileHandler {
+    boolean writeUserInputToFile();
+    void readFromFile(String fileName);
+}
+
+public class FileHandler implements IFileHandler {
+    @Override
+    public boolean writeUserInputToFile() {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.println("Enter file's name:");
+            String fileName = scanner.nextLine();
+
+            if (fileName.isEmpty() || fileName.contains(File.separator)) {
+                System.out.println("Invalid file name.");
+                return false;
+            }
+
+            String path = "C:\\temp\\" + fileName + ".txt";
+
+            File newFile = new File(path);
+            if (newFile.createNewFile()) {
+                System.out.println("File " + newFile.getName() + " is created successfully.");
+
+                System.out.println("Enter text:");
+                String inputLine = scanner.nextLine().trim();
+
+                String[] words = inputLine.split("\\s+");
+
+                StringBuilder contentBuilder = new StringBuilder();
+                for (int i = 0; i < words.length; i++) {
+                    contentBuilder.append(words[i]).append(" ");
+                    if ((i + 1) % 4 == 0) {
+                        contentBuilder.append("\n");
+                    }
+                }
+
+                String content = contentBuilder.toString().trim();
+
+                try (FileWriter fileWriter = new FileWriter(path)) {
+                    fileWriter.write(content);
+                }
+
+                System.out.println("Content is successfully wrote to the file.");
+                return true;
+            } else {
+                System.out.println("File is already exist in the directory.");
+            }
+        } catch (IOException exception) {
+            System.out.println("An unexpected error is occurred.");
+            exception.printStackTrace();
+        }
+
+        return false;
+    }
+
+    @Override
+    public void readFromFile(String fileName) {
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 //        DirectoryLister directoryLister = new DirectoryLister();
 //        directoryLister.printDirectory();
+
+        FileHandler fileHandler = new FileHandler();
+        fileHandler.writeUserInputToFile();
     }
 }
